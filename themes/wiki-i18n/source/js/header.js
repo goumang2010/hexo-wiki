f4ab7917928b38be4be9250581fa08b39d164b5f
goumang2010 <goumang2010@live.com>
2016-09-01 00:40:32 +0800

    modify theme and make it run

--- themes/wiki-i18n/source/js/header.js
+++ themes/wiki-i18n/source/js/header.js
@@ -1,21 +1,21 @@
 (function () {
-  var form = document.getElementById('header-search-form');
-  var input = document.getElementById('header-search-input');
-  form.addEventListener('submit', function (e) {
+  const form = document.getElementById('header-search-form');
+  const input = document.getElementById('header-search-input');
+  form.addEventListener('submit', (e) => {
     e.preventDefault();
-    var text = input.value.trim();
+    let text = input.value.trim();
     if (text === '') {
       return;
     }
-    var googleSearchPrefix = 'https://www.google.com/#newwindow=1&q=';
-    var siteUrl = window.HEXO_DATA.config.url;
-    window.open(googleSearchPrefix + 'site:' + siteUrl + '+' + text, '_blank');
+    let googleSearchPrefix = 'https://www.google.com/#newwindow=1&q=';
+    let siteUrl = window.HEXO_DATA.config.url;
+    window.open(`${googleSearchPrefix}site:${siteUrl}+${text}`, '_blank');
   });
 
-  var select = document.getElementById('header-lang-select');
-  select.addEventListener('change', function (e) {
+  let select = document.getElementById('header-lang-select');
+  select.addEventListener('change', (e) => {
     e.preventDefault();
-    var option = e.target.querySelector('[value="' + e.target.value + '"]');
+    let option = e.target.querySelector(`[value="${e.target.value}"]`);
     location.href = option.dataset.href;
   });
 }());
