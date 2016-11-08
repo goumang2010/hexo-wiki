4ff6dcb5e079d346707ef9732fa341e98945d932
goumang2010 <goumang2010@live.com>
2016-11-09 01:50:48 +0800

    (split) fix lint

--- scripts/hexo-filter.js
+++ scripts/hexo-filter.js
@@ -1,8 +1,9 @@
-var yaml = require('yaml-front-matter');
+/* global hexo */
+const yaml = require('yaml-front-matter');
 
-hexo.extend.filter.register('after_post_render', function(data) {
-  var github = yaml.loadFront(data.raw).github;
-  if (github !== undefined){
+hexo.extend.filter.register('after_post_render', (data) => {
+  let github = yaml.loadFront(data.raw).github;
+  if (github !== undefined) {
     data.github = github;
   }
   return data;
