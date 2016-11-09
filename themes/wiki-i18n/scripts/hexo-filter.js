f2521143abd82717ab894cea63d1afe9881c3b59
goumang2010 <goumang2010@live.com>
2016-11-10 01:49:11 +0800

    search

--- themes/wiki-i18n/scripts/hexo-filter.js
+++ themes/wiki-i18n/scripts/hexo-filter.js
@@ -8,3 +8,11 @@ hexo.extend.filter.register('after_post_render', (data) => {
   }
   return data;
 });
+
+hexo.extend.filter.register('after_about_render', (data) => {
+  let github = yaml.loadFront(data.raw).github;
+  if (github !== undefined) {
+    data.github = github;
+  }
+  return data;
+});
