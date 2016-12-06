ad07c0f7241d518639b578da67b90b298bb2c62d
goumang2010 <goumang2010@live.com>
2016-12-07 00:33:16 +0800

    (split) fix lint

--- source/js/src/utils/common.js
+++ source/js/src/utils/common.js
@@ -13,44 +13,47 @@ export const bindEvent = (element, event, cb, capture) => {
 };
 
 export const jsonToQuery = function (a) {
-	var add = function (s, k, v) {
-		v = typeof v === 'function' ? v() : v === null ? '' : v === undefined ? '' : v;
-		s[s.length] = encodeURIComponent(k) + '=' + encodeURIComponent(v);
-	}, buildParams = function (prefix, obj, s) {
-		var i, len, key;
+    const add = function (s, k, v) {
+        v = typeof v === 'function' ? v() : v === null ? '' : v === undefined ? '' : v;
+        s[s.length] = encodeURIComponent(k) + '=' + encodeURIComponent(v);
+    };
+    const buildParams = function (prefix, obj, s) {
+        let i;
+        let len;
+        let key;
 
-		if (Object.prototype.toString.call(obj) === '[object Array]') {
-			for (i = 0, len = obj.length; i < len; i++) {
-				buildParams(prefix + '[' + (typeof obj[i] === 'object' ? i : '') + ']', obj[i], s);
-			}
-		} else if (obj && obj.toString() === '[object Object]') {
-			for (key in obj) {
-				if (obj.hasOwnProperty(key)) {
-					if (prefix) {
-						buildParams(prefix + '[' + key + ']', obj[key], s, add);
-					} else {
-						buildParams(key, obj[key], s, add);
-					}
-				}
-			}
-		} else if (prefix) {
-			add(s, prefix, obj);
-		} else {
-			for (key in obj) {
-				add(s, key, obj[key]);
-			}
-		}
-		return s;
-	};
-	return buildParams('', a, []).join('&').replace(/%20/g, '+');
+        if (Object.prototype.toString.call(obj) === '[object Array]') {
+            for (i = 0, len = obj.length; i < len; i++) {
+                buildParams(prefix + '[' + (typeof obj[i] === 'object' ? i : '') + ']', obj[i], s);
+            }
+        } else if (obj && obj.toString() === '[object Object]') {
+            for (key in obj) {
+                if (obj.hasOwnProperty(key)) {
+                    if (prefix) {
+                        buildParams(prefix + '[' + key + ']', obj[key], s, add);
+                    } else {
+                        buildParams(key, obj[key], s, add);
+                    }
+                }
+            }
+        } else if (prefix) {
+            add(s, prefix, obj);
+        } else {
+            for (key in obj) {
+                add(s, key, obj[key]);
+            }
+        }
+        return s;
+    };
+    return buildParams('', a, []).join('&').replace(/%20/g, '+');
 };
 
-export const JSONParse = function() {
-		if (window.JSON) {
-			return JSON.parse;
-		} else {
-			return function(jsonstr) {
-				return (new Function("return" + jsonstr))();
-			};
-		}
-	}();
+export const JSONParse = function () {
+    if (window.JSON) {
+        return JSON.parse;
+    } else {
+        return function (jsonstr) {
+            return (new Function("return" + jsonstr))();
+        };
+    }
+}();
