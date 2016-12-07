880027787d97c39c4958a79be929ca51c41c6f6d
goumang2010 <goumang2010@live.com>
2016-12-07 18:56:39 +0800

    lint

--- themes/wiki-i18n/source/js/src/utils/class.js
+++ themes/wiki-i18n/source/js/src/utils/class.js
@@ -1,7 +1,5 @@
-
 function trimCls(c) {
-    return (typeof (c) === 'string' ? 
-    c.trim().replace(/\s+/g, ' ').split(' ') : c);
+    return (typeof (c) === 'string' ? c.trim().replace(/\s+/g, ' ').split(' ') : c);
 }
 
 function CtrlClass(ele) {
@@ -12,41 +10,36 @@ function CtrlClass(ele) {
 
 CtrlClass.prototype.show = function() {
     this.ele.style.display = 'block';
-}
+};
 
 CtrlClass.prototype.hide = function() {
     this.ele.style.display = 'none';
-}
+};
 
 CtrlClass.prototype.hasClass = function(c) {
     return trimCls(c).every((v) => {
-                return !!this.classList.contains(v);
-            });
-}
-
+        return !!this.classList.contains(v);
+    });
+};
 
 CtrlClass.prototype.addClass = function(c) {
     trimCls(c).forEach((v) => {
-            if (!this.hasClass(v)) {
-                this.classList.add(v);
-            }
-        });
-}
+        if (!this.hasClass(v)) {
+            this.classList.add(v);
+        }
+    });
+};
 
-/**
- * removeClass
- * @param (element, 'c1 c2 c3')
- */
 CtrlClass.prototype.removeClass = function(c) {
     trimCls(c).forEach((v) => {
         this.classList.remove(v);
     });
-}
+};
 
 CtrlClass.prototype.toggleClass = function(c) {
     trimCls(c).forEach((v) => {
         this.classList.toggle(v);
     });
-}
+};
 
 export default CtrlClass;
