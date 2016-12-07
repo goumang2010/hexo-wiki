ebf2ff12dccefcfa25b3ab1b02e45be38d96cb4a
goumang2010 <goumang2010@live.com>
2016-12-07 23:39:31 +0800

    (split) fix ghost click

--- source/js/src/mobile.js
+++ source/js/src/mobile.js
@@ -1,33 +1,21 @@
 // Mobile nav
-// console.log($);
 import {CtrlClass, bindClickEvent} from './utils';
 
 const $container = document.body;
 const bodyClass = new CtrlClass($container);
-let isMobileNavAnim = false;
-let mobileNavAnimDuration = 200;
-let startMobileNavAnim = function () {
-    isMobileNavAnim = true;
-};
 
-let stopMobileNavAnim = function () {
-    setTimeout(function () {
-        isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
-};
 let mnavClass = new CtrlClass(document.querySelector('#mobile-nav'));
-
+let show = false;
 bindClickEvent(document.querySelector('#main-nav-toggle'), function () {
-    if (isMobileNavAnim) return;
-    startMobileNavAnim();
+    if (show) return;
     bodyClass.toggleClass('mobile-nav-on');
     mnavClass.show();
-    stopMobileNavAnim();
+    show = true;
 });
 
-// $(input).val('kkkk');
 bindClickEvent($container, function () {
-    if (isMobileNavAnim || !bodyClass.hasClass('mobile-nav-on')) return;
+    if (show === false) return;
     bodyClass.removeClass('mobile-nav-on');
     mnavClass.hide();
+    show = false;
 });
