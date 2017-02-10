2a8a36b74399dac08cba349a332691015891b384
goumang2010 <goumang2010@live.com>
2017-02-10 15:57:42 +0800

    fix index link

---
+++ source/js/src/href.js
@@ -0,0 +1,25 @@
+import {searchKey} from './search.js';
+import {bindClickEvent} from './utils';
+
+
+
+function getUrl(name, callback) {
+    return searchKey(name, (results) => {
+        let res;
+        if ((res = results[0]) && (res = res.path)) {
+            callback(res);
+        }
+    });
+} 
+
+bindClickEvent(document.body, function(e) {
+    let target = e.target;
+    if(target.tagName.toLowerCase() === 'a') {
+        let href;
+        if(/^[^#/]+$/.test(href = target.getAttribute('href'))) {
+            getUrl(href, (res) => {
+                target.href = res;
+            });
+        }
+    }
+});
--- source/js/src/index.js
+++ source/js/src/index.js
@@ -3,3 +3,4 @@ import './polyfills';
 import './lang.js';
 import './mobile.js';
 import './search.js';
+import './href.js';
